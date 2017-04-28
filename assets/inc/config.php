379074109fa0060389d000a87e9bd880b945750e
Uğur <xaron@blazelayer.com>
2017-04-28 22:44:01 +0300

    Update config.php

--- assets/inc/config.php
+++ assets/inc/config.php
@@ -1,4 +1,4 @@
 <?php
-$fransa = array('ip' => '193.70.22.23', 'queryName' => 'serveradmin', 'queryPassword' => 'ErxuxnWE', 'queryPort' => '10011');
+$fransa = array('ip' => '127.0.0.2', 'queryName' => 'serveradmin', 'queryPassword' => 'BirÜçİkiYedi', 'queryPort' => '10011');
 $almanya = array('ip' => '127.0.0.1', 'queryName' => 'serveradmin', 'queryPassword' => 'BirÜçİkiSekiz', 'queryPort' => '10011');
 ?>
