892d042b660a7e137b91a80362edbc6a79634223
Uğur <xaron@blazelayer.com>
2017-04-28 22:44:40 +0300

    Update functions.php

--- assets/inc/functions.php
+++ assets/inc/functions.php
@@ -1,6 +1,6 @@
 <?php
 class genel{
-	function mesaj($icerik, $durum, $sure){
+	public function mesaj($icerik, $durum, $sure){
 		if(isset($icerik) && isset($durum)){
 			echo "<script type='text/javascript'>$(document).ready(function(){ xaron.Mesaj('bottom','left','$icerik',$durum,$sure); });</script>";
 		}
